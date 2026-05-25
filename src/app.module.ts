f9bf87d56128ef1d21350a3c0e92dba81201966f
Iago-Santos-Sousa <iago.santos.sousa@gmail.com>
2026-05-25 01:04:27 -0300

    refactor(app): standardize import formatting and enhance OrderItem entity with created_at and updated_at fields

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,32 @@
-import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
-import { LoggerMiddleware } from './common/middleware/logger.middleware';
-import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { DataSource } from 'typeorm';
-import { ThrottlerGuard } from '@nestjs/throttler';
-import { APP_GUARD } from '@nestjs/core';
-import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
-import { OrdersModule } from './orders/orders.module';
-import { AddressModule } from './address/address.module';
-import { dataSourceOptions } from '../data-source';
+import { Module, NestModule, MiddlewareConsumer } from "@nestjs/common";
+import { LoggerMiddleware } from "./common/middleware/logger.middleware";
+import { ConfigModule } from "@nestjs/config";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { TypeOrmModule, TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { DataSource } from "typeorm";
+import { ThrottlerModule, ThrottlerGuard } from "@nestjs/throttler";
+import { APP_GUARD } from "@nestjs/core";
+import { UserModule } from "./user/user.module";
+import { AuthModule } from "./auth/auth.module";
+import { OrdersModule } from "./orders/orders.module";
+import { AddressModule } from "./address/address.module";
+import { dataSourceOptions } from "../data-source";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: '.env',
+      envFilePath: ".env",
     }),
 
     TypeOrmModule.forRoot(dataSourceOptions as TypeOrmModuleOptions),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 10,
+      },
+    ]),
     AuthModule,
     UserModule,
     OrdersModule,
@@ -39,6 +45,6 @@ export class AppModule implements NestModule {
   constructor(private dataSource: DataSource) {}
 
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes('/');
+    consumer.apply(LoggerMiddleware).forRoutes("/");
   }
 }
